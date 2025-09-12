77d8a43be4a6a4065fb0269c20561707f7c3b9a0
mi93-git <marcel.ihme@gmail.com>
2025-09-12 19:21:32 +0200

    api

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# alternative: from rest_framework.decorators import api_view
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
+# create list  (GET / POST)
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# read/change/delete single object (GET / PUT/PATCH / DELETE)
+class SingleMenuView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
